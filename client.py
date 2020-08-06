5efda27c5a3f5b984cf6f4b5d46892fcadbab2b1
azarudeenpn <azharudheenpn@hotmail.com>
2020-08-06 09:58:20 +0530

    added response

--- client.py
+++ client.py
@@ -5,5 +5,6 @@ url = sys.argv[1]
 caption = input("Enter caption: ")
 category = input("Enter category to select tags: ")
 payload = {'url': url, 'caption': caption, 'category': category}
-server_addr = "http://192.168.0.198:5000/"
+server_addr = "http://127.0.0.1:5000/"
 response = requests.get(server_addr, params=payload)
+print(response.text)
